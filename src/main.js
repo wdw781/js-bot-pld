d1eef119ec211568f7230d479e9fbeec3bf32715
wdw <wernercancel018@gmail.com>
2023-01-14 20:02:05 +0800

    add slash command -- ping

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ dotenv.config()
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
